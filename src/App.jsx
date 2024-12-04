390f471b46f585711aee3aaff6ff4c69e387cbad
mavix21 <marcevizcarra2109@hotmail.com>
2024-12-03 23:58:59 -0500

    Add confetti effect and reset functionality to the game

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,20 @@ import './App.css';
 import { Dice } from './components/Dice';
 import { createRandomNumbers } from './lib';
 import { useState } from 'react';
-import { useEffect } from 'react';
+import Confetti from 'react-confetti';
+
+const generateAllNewDices = (amount) =>
+  createRandomNumbers(amount).map((value) => ({
+    id: nanoid(),
+    value,
+    isHeld: false,
+  }));
 
 function App() {
-  const [diceValues, setDiceValues] = useState(
-    createRandomNumbers(10).map((value) => ({
-      id: nanoid(),
-      value,
-      isHeld: false,
-    })),
+  const [diceValues, setDiceValues] = useState(() => generateAllNewDices(10));
+
+  const gameWon = diceValues.every(
+    (dice) => dice.isHeld && dice.value === diceValues[0].value,
   );
 
   const holdDie = (id) => {
@@ -31,14 +36,14 @@ function App() {
     );
   };
 
-  // useEffect(() => {
-  //   console.log(diceValues);
-  // }, [diceValues]);
+  const resetGame = () => {
+    setDiceValues(generateAllNewDices(10));
+  };
 
   return (
     <div className="h-svh bg-[#0B2434] p-6 flex flex-col justify-center items-center">
       <section className="bg-[#F5F5F5] space-y-2 flex flex-col p-4 text-center rounded-xl w-full max-w-[32rem] aspect-square">
-        <h1 className="text-4xl font-semibold">Tenzies</h1>
+        <h1 className="text-4xl font-bold">Tenzies</h1>
         <p className="">
           Roll until all dice are the same. Click each die to freeze it at its
           current value between rolls.
@@ -56,10 +61,17 @@ function App() {
         </div>
         <button
           className="py-2 px-4 bg-blue-600 rounded-md text-white text-xl w-full max-w-40 mx-auto cursor-pointer"
-          onMouseDown={() => rollDice()}
+          onMouseDown={() => (gameWon ? resetGame() : rollDice())}
         >
-          Roll
+          {gameWon ? 'Play again' : 'Roll'}
         </button>
+        {gameWon && (
+          <Confetti
+            width={window.innerWidth}
+            height={window.innerHeight}
+            recycle={false}
+          />
+        )}
       </section>
     </div>
   );
